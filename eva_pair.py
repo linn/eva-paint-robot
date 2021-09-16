deffad7f887fdefaaa142ed5adfefa2ab7ab03b5
Moronibot <69640923+Moronibot@users.noreply.github.com>
2021-09-16 17:38:10 +0100

    Twin control mostly done

--- eva_pair.py
+++ eva_pair.py
@@ -1,13 +1,64 @@
-import evasdk
+from evasdk import Eva, RobotState, EvaError
 
 
 class EvaPair:
-    def __init__(self, robot_1: dict, robot_2: dict):
-        self.left_robot = evasdk.Eva(robot_1['ip'], robot_1['token'])
-        self.right_robot = evasdk.Eva(robot_2['ip'], robot_2['token'])
+    def __init__(self, robot_l: Eva, robot_r: Eva):
+        self.robot_pair = [robot_l, robot_r]
+        self.reset_attempts = 3
+
+    def check_and_reset_state(self, eva: Eva, recur_count: int = 0) -> bool:
+        eva_name = eva.name()['name']
+        eva_state = eva.data_snapshot_property('control')['state']
+        if eva_state == RobotState.READY:
+            return True
+        elif eva_state == RobotState.ERROR.value and recur_count < self.reset_attempts:
+            print(f"{eva_name}: Attempt number {recur_count + 1} to reset...")
+            eva.control_reset_errors()
+            self.check_and_reset_state(eva, recur_count=(recur_count + 1))
+        elif eva_state == RobotState.DISABLED.value:
+            raise EvaError(f"Unable to reset robot {eva_name} - CHECK E-STOP")
+        elif eva_state == RobotState.ERROR.value and recur_count == self.reset_attempts:
+            raise EvaError(f"Unable to reset robot: {eva.name}")
+
+    def check_in_ready_state(self, eva) -> bool:
+        eva_name = eva.name()['name']
+        eva_state = eva.data_snapshot_property('control')['state']
+        if eva_state == RobotState.READY:
+            return True
+        print(f"{eva.name()['name']}: READY TO RUN")
+        return True
+
+    def send_home_pair(self) -> None:
+        for eva in self.robot_pair:
+            with eva.lock():
+                self.check_and_reset_state(eva)
+        for eva in self.robot_pair:
+            with eva.lock():
+                print(f"{eva.name()['name']}: SENDING HOME")
+                eva.control_home(wait_for_ready=True)
+
+    @staticmethod
+    def set_active_toolpath(eva: Eva, toolpath_id: int) -> None:
+        eva.toolpaths_use_saved(toolpath_id)
+
+    def run_toolpath_pair(self):
+        for eva in self.robot_pair:
+            self.check_in_ready_state(eva)
+        for eva in self.robot_pair:
+            with eva.lock():
+                print(f"{eva.name()['name']}: STARTING TOOLPATH")
+                eva.control_run(loop=0, wait_for_ready=False, mode='automatic')
+
+    def stop_toolpath_pair(self) -> None:
+        for eva in self.robot_pair:
+            with eva.lock():
+                eva.control_stop_loop(wait_for_ready=True)
 
 
 if __name__ == '__main__':
-    robot_1_details = {'ip': '172.16.16.2', 'token': ''}
-    robot_2_details = {'ip': '172.16.16.3', 'token': ''}
-    eva_x2 = EvaPair(robot_1_details, robot_2_details)
+    robot_1_eva = Eva('10.10.60.175', '357abe95ba3b3b412a09f765f5395ae533616eb7')
+    robot_2_eva = Eva('10.10.60.189', '19a397843a066a8838d62630c88f060db76fd25b')
+    eva_x2 = EvaPair(robot_1_eva, robot_2_eva)
+    eva_x2.send_home_pair()
+    eva_x2.run_toolpath_pair()
+    eva_x2.stop_toolpath_pair()
--- new_linn.py
+++ new_linn.py
@@ -7,15 +7,32 @@ Alastair Bennett    10:52 AM
 4) Run by toolpath name
 5) left and right robot
 """
-import evasdk
+from evasdk import Eva
 import pickle
+import eva_pair
 
 
 class LinnTwinRobotApp:
-    def __init__(self, robot_1: dict, robot_2: dict):
-        self.left_robot = evasdk.Eva(robot_1['ip'], robot_1['token'])
-        self.right_robot = evasdk.Eva(robot_2['ip'], robot_2['token'])
+    def __init__(self, robot_left: dict, robot_right: dict):
+        self.robot_left = Eva(robot_left['ip'], robot_left['token'])
+        self.robot_right = Eva(robot_right['ip'], robot_right['token'])
+        self.robot_order = {'left': self.robot_left, 'right': self.robot_right}
+        self.eva_pair = eva_pair.EvaPair(self.robot_left, self.robot_right)
         self.barcode_tp_pkl = "barcode_toolpath.pkl"
+
+    @staticmethod
+    def toolpath_list_name(eva: Eva, left_right: str) -> None:
+        toolpath_list_name_only = []
+        print(f"Available toolpaths - Number to enter to set toolpath for {left_right} robot")
+        for toolpaths in eva.toolpaths_list():
+            print(f"{toolpaths['name']}    -    {toolpaths['id']}")
+            toolpath_list_name_only.append(toolpaths['name'])
+
+    @staticmethod
+    def input_toolpath_name(eva: Eva, left_right: str, toolpath_id: int) -> None:
+        with eva.lock():
+            print(f"{eva.name()['name']} ({left_right}): Setting active toolpath")
+            eva.toolpaths_use_saved(toolpath_id)
 
     def assign_barcode(self):
         barcode_toolpath: dict = {}
@@ -29,29 +46,24 @@ class LinnTwinRobotApp:
 
             while not barcode:
                 barcode = input("\nScan Barcode... (q to quit):")
-
                 if barcode in barcode_toolpath:
                     override = input("Barcode already registered. Override? (y/n)")
-
                     if override == "n":
                         barcode = None
-
                 if barcode == "q":
                     running = False
 
             while not toolpath:
-                toolpath = input("Input Tootpath ID (l for list):")
+                toolpath = input("Input Toolpath ID (l for list):")
                 if toolpath == "l":
-                    print(self.left_robot.toolpaths_list())
-
+                    print(self.robot_left.toolpaths_list())
                     toolpath = int(input("Input Tootpath ID:"))
 
             barcode_toolpath[barcode] = toolpath
-
         print(barcode_toolpath)
-        a_file = open(self.barcode_tp_pkl, "ab")  # EM: Changed
-        pickle.dump(barcode_toolpath, a_file)
-        a_file.close()
+
+        with open(self.barcode_tp_pkl, "ab") as pkl_file:  # EM: Changed
+            pickle.dump(barcode_toolpath, pkl_file)
 
     def run_barcode_toolpath(self):
         running = True
@@ -60,18 +72,16 @@ class LinnTwinRobotApp:
 
         while running:
             barcode = input("\nScan Barcode... (q to quit):")
-
             if barcode == "q":
                 running = False
-
-            if barcode in barcode_toolpath:
+            elif barcode in barcode_toolpath:
                 toolpath_id = barcode_toolpath.get(barcode)
 
-                with eva.lock():
+                """with eva.lock():
                     eva.control_wait_for_ready()
                     eva.toolpaths_use_saved(int(toolpath_id))  # EM: Changed
                     eva.control_home()
-                    eva.control_run(loop=0, mode='automatic')  # EM: Changed
+                    eva.control_run(loop=0, mode='automatic')  # EM: Changed"""
             else:
                 print("Barcode Unregistered!")
 
@@ -85,8 +95,16 @@ class LinnTwinRobotApp:
         else:
             raise ValueError(f"Unexpected input: {ass_or_run}")
 
+    def set_left_right_robot_toolpaths(self):
+        for left_right in self.robot_order:
+            self.toolpath_list_name(self.robot_order[left_right], left_right)
+            toolpath_num = int(input("Enter toolpath number: "))
+            self.input_toolpath_name(self.robot_order[left_right], left_right, toolpath_num)
+
+
 
 if __name__ == '__main__':
-    robot_1_details = {'ip': '172.16.16.2', 'token': ''}
-    robot_2_details = {'ip': '172.16.16.3', 'token': ''}
+    robot_1_details = {'ip': '10.10.60.175', 'token': '357abe95ba3b3b412a09f765f5395ae533616eb7'}
+    robot_2_details = {'ip': '10.10.60.189', 'token': '19a397843a066a8838d62630c88f060db76fd25b'}
     app = LinnTwinRobotApp(robot_1_details, robot_2_details)
+    app.set_left_right_robot_toolpaths()
