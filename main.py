92b25fcf33f17c86b7915de534c20900c794cea5
Kyle Gordon <kyle@lodge.glasgownet.com>
2021-10-27 15:48:39 +0100

    Use vars

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from new_linn import LinnTwinRobotApp
+import os
 import sys
 """ Main file to run application """
 
@@ -13,8 +14,8 @@ pipenv run python main.py --headerless
 """
 
 if __name__ == '__main__':
-    robot_1_details = {'ip': '10.10.60.175', 'token': '357abe95ba3b3b412a09f765f5395ae533616eb7'}
-    robot_2_details = {'ip': '10.10.60.189', 'token': '19a397843a066a8838d62630c88f060db76fd25b'}
+    robot_1_details = {'ip': os.environ['ROBOT1IP'], 'token': os.environ['ROBOT1API']}
+    robot_2_details = {'ip': os.environ['ROBOT2IP'], 'token': os.environ['ROBOT2API']}
     app = LinnTwinRobotApp(robot_1_details, robot_2_details)
     if '--headerless' in sys.argv:
         app.scan_and_run_barcode(headerless=True)
